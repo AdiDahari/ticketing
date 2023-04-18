1db7fe6fe3a3404368bc4410dc3f27e541454067
Adi Dahari <dahariadi16@gmail.com>
2023-04-18 19:39:07 +0300

    common library published and integrated

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
-import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError, validateRequest } from "@adtixit/common";
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
 import { PasswordManager } from "../services/password";
 
 const router = express.Router();
